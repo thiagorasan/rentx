f2a070fc7670ed74d44b330888171b6c6958212f
thiagorasan <trs.tiba@gmail.com>
2021-12-22 23:47:37 -0300

    Finalizando CarDetails com Button

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import BackButton from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
 
 import speedSvg from '../../assets/speed.svg';
 import accelerationSvg from '../../assets/acceleration.svg';
@@ -26,6 +28,7 @@ import {
     Price,
     About,
     Accessories,
+    Footer
 } from './styles';
 
 export function CarDetails() {
@@ -71,6 +74,9 @@ export function CarDetails() {
                 </About>
 
             </Content>
+            <Footer>
+                <Button title="Confirmar"/>
+            </Footer>
         </Container>
     )
 }
